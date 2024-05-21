cee3651b80c3367425114da9d2ad769ba1aa2f0c
LexKRS <lexocode@yandex.ru>
2024-05-21 15:53:47 +0300

    Added card for products

--- store/products/views.py
+++ store/products/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
+from products.models import ProductsCategory, Product
+
 # Create your views here.
 
 def index(request):
     context = {
-        'title': 'Store'
+        'title': 'Store',
+
     }
     return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store',
+        'categories' : ProductsCategory.objects.all(),
+        'products' : Product.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
